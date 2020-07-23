3e37f9deee9a24d3ec087f2e61ac51029ea19731
Dana G <32311532+dastryyy@users.noreply.github.com>
2020-07-22 20:24:05 -0400

    ur tellin me a shrimp fried that rice

--- app/src/main/java/com/ewgf/cramirez/framedataapp/MainActivity.java
+++ app/src/main/java/com/ewgf/cramirez/framedataapp/MainActivity.java
@@ -2,13 +2,32 @@ package com.ewgf.cramirez.framedataapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView myListView;
+    String[] move_names;
+    String[] move_inputs;
+    String[] move_speeds;
+    String[] move_on_blocks;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.move_select);
+
+        Resources res = getResources();
+        myListView = (ListView) findViewById(R.id.myListView);
+        move_names = res.getStringArray(R.array.move_names);
+        move_inputs = res.getStringArray(R.array.move_inputs);
+        move_speeds = res.getStringArray(R.array.move_speeds);
+        move_on_blocks = res.getStringArray(R.array.move_on_blocks);
+
+        ItemAdapter itemAdapter = new ItemAdapter(this, move_names, move_inputs, move_speeds, move_on_blocks);
+        myListView.setAdapter(itemAdapter);
     }
 }
