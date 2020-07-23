363d6f36d4ef7fc753f8ebafed6dcd16d9acea02
Cesar <cesaram5@yahoo.com>
2020-07-22 19:18:29 -0700

    added character portrait and name to move select layout

--- app/src/main/java/com/ewgf/cramirez/framedataapp/MainActivity.java
+++ app/src/main/java/com/ewgf/cramirez/framedataapp/MainActivity.java
@@ -3,8 +3,12 @@ package com.ewgf.cramirez.framedataapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.Display;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +18,7 @@ public class MainActivity extends AppCompatActivity {
     String[] move_inputs;
     String[] move_speeds;
     String[] move_on_blocks;
+    int char_portrait;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +31,35 @@ public class MainActivity extends AppCompatActivity {
         move_inputs = res.getStringArray(R.array.move_inputs);
         move_speeds = res.getStringArray(R.array.move_speeds);
         move_on_blocks = res.getStringArray(R.array.move_on_blocks);
+        char_portrait = R.drawable.leona_portrait;
 
         ItemAdapter itemAdapter = new ItemAdapter(this, move_names, move_inputs, move_speeds, move_on_blocks);
         myListView.setAdapter(itemAdapter);
+
+        ImageView img = (ImageView) findViewById(R.id.charPortraitImageView);
+        scaleImage(img, char_portrait);
+
+    }
+
+    private void scaleImage(ImageView img, int pic)
+    {
+        Display screen = getWindowManager().getDefaultDisplay();
+        BitmapFactory.Options options = new BitmapFactory.Options();
+
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeResource(getResources(), pic, options);
+
+        int imageWidth = options.outWidth;
+        int screenWidth = screen.getWidth();
+
+        if (imageWidth > screenWidth)
+        {
+            int ratio = Math.round((float) imageWidth / (float) screenWidth);
+            options.inSampleSize = ratio;
+        }
+
+        options.inJustDecodeBounds = false;
+        Bitmap scaledImage = BitmapFactory.decodeResource(getResources(), pic, options);
+        img.setImageBitmap(scaledImage);
     }
 }
